a00cd1d5ad210f4e831e005b01173c5ffbb6b5d1
lmp <dev.larpon@gmail.com>
2019-07-07 23:28:57 +0200

    Introduce DebugCell to keep base cell light weight

---
+++ DebugCell.gd
@@ -0,0 +1,22 @@
+extends "./Cell.gd"
+
+var debug := true
+
+onready var grid = get_parent()
+
+func _id(): return str(xy)
+
+func _ready():
+	if not is_instance_valid(self): return
+	
+	if debug:
+		var label = get_node('ColorRect/Label')
+		var color_rect = get_node('ColorRect')
+		if not is_instance_valid(label) or not is_instance_valid(color_rect): return
+		
+		label.set_text(_id())
+		color_rect.color = Color(rand_range(0.0,1.0),rand_range(0.0,1.0),rand_range(0.0,1.0),0.5)
+	$ColorRect.rect_size = Vector2(grid.cell_size.x,grid.cell_size.y)
+
+func _process(delta):
+	if debug: get_node('ColorRect/Label').set_text(_id())
