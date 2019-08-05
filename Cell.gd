8ff43490e3bbf3646be606a6c3ab4091c8a96e8c
lmp <dev.larpon@gmail.com>
2019-08-05 23:22:00 +0200

    Remove used signal and comment

--- Cell.gd
+++ Cell.gd
@@ -2,14 +2,12 @@ extends Node2D
 
 var xy := Vector2(0,0) setget set_xy
 
-#signal swapped()
 signal xy_changed(new, old)
 
 onready var grid := get_parent()
 
 func set_xy(new_xy):
 	if xy.x != new_xy.x or xy.y != new_xy.y:
-	#if xy != new_xy:
 		var _xy = Vector2( xy.x, xy.y )
 		xy = new_xy
 		emit_signal("xy_changed",xy,_xy)
